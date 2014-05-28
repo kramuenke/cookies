02c33ba24947d8c7798914bccb71bed2ee7d8689
Kai Ramuenke <kai.ramuenke@gmail.com>
2014-05-28 15:32:31 +1000

    damn typos

--- web.rb
+++ web.rb
@@ -1,7 +1,7 @@
 require 'sinatra'
 
 get '/set' do
-  response.set_cookie("Bob", :value => 'foo', :path => '/login', :secure => true, :htt_only => true)
+  response.set_cookie("Bob", :value => 'foo', :path => '/login', :secure => true, :http_only => true)
   "bla"
 end
 
